ad65acb596addace12a27d15a61a3db16a531283
EGYuda23 <egyyudanugraha@gmail.com>
2022-11-24 16:40:51 +0700

    add solution

--- public/js/classification.js
+++ public/js/classification.js
@@ -9,10 +9,12 @@ const DISEASE = {
             Akibat
         </div>
         <div class="collapse-content">
-        <p>Pertumbuhan tanaman terhambat</p>
-        <p>Butir-butir padi kurang bernas</p>
-        <p>Kualitas biji rendah</p>
-        <p>Persentase beras pecah tinggi</p>
+          <ul class="list-disc ml-5">
+            <li>Pertumbuhan tanaman terhambat</li>
+            <li>Butir-butir padi kurang bernas</li>
+            <li>Kualitas biji rendah</li>
+            <li>Persentase beras pecah tinggi</li>
+          </ul>
         </div>
     </div>
     
@@ -24,20 +26,20 @@ const DISEASE = {
         <div class="collapse-content">
         <p>
           <p class="font-semibold">Pratanam</p>
-          <ul>
+          <ul class="list-disc ml-5">
             <li>Sanitasi tanaman inang</li>
             <li>Pemilihan varietas tahan sesuai sebaran ras</li>
           </ul>
           
           <p class="font-semibold">Pesemaian</p>
-          <ul>
+          <ul class="list-disc ml-5">
             <li>Penggunaan benih sehat</li>
             <li>Sanitasi inang pada saluran irigasi</li>
             <li>Hindari penggenangan terlalu dalam</li>
           </ul>        
         
           <p class="font-semibold">Tanaman Muda (Tanam - Anakan maksimum)</p>
-          <ul>
+          <ul class="list-disc ml-5">
             <li>Pemupukan berimbang sesuai anjuran setempat</li>
             <li>Sanitasi rerumputan sumber patogen</li>
             <li>Pengeringan lahan secara berkala, yaitu 1 hari diairi dan 3-4 hari dikeringkan, terutama pada daerah endemik serangan penyakit</li>
@@ -56,9 +58,13 @@ const DISEASE = {
             Akibat
         </div>
         <div class="collapse-content">
-          <p>-</p>
-          <p>-</p>
-          <p>-</p>
+          <ul class="list-disc ml-5">
+            <li>Bercak pada daun yang khas berbentuk oval</li>
+            <li>Bercak berwarna coklat dilingkari dengan warna abu bagian tengah becak bulat berwarna putih</li>
+            <li>Gejala yang masih muda berupa binik-bintik coklat atau coklat keabuan</li>
+            <li>Pada varietas peka bercak akan lebih lebar berukuran mencapai 1 cm atau lebih</li>
+            <li>Jika bercak memenuhi permukaan daun maka akan berakibat daun menjadi layu</li>
+          </ul>
         </div>
     </div>
     
@@ -68,9 +74,16 @@ const DISEASE = {
           Solusi
         </div>
         <div class="collapse-content">
-          <p>-</p>
-          <p>-</p>
-          <p>-</p>
+          <ul class="list-disc ml-5">
+            <li>Jarak tanam yang tidak terlalu rapat terutama saat musim hujan</li>
+            <li>Jika perlu gunakan cara tanam sistem legowo</li>
+            <li>Jangan gunakan urea yang berlebih dan imbangi dengan unsur K</li>
+            <li>Aplikasi fungisida pada daun tanaman padi, contoh: antracol, dithane, dan fungisida kontak lain sebagai pencegahnya. Jika sudah terserang gunakan fungisida sistemik seperti score, anvil, folicur, Nativo, opus, indar dll.</li>
+            <li>Penanaman varietas tahan, seperti Ciherang dan Membrano</li>
+            <li>Pemupukan berimbang yang lengkap, yaitu 250 kg urea, 100 kg SP36, dan 100 kg KCl per ha</li>
+            <li>Penyemprotan fungisida dengan bahan aktif</li>
+            <li>Difenoconazol</li>  
+          </ul>
         </div>
     </div>
     `
@@ -84,9 +97,11 @@ const DISEASE = {
             Akibat
         </div>
         <div class="collapse-content">
-          <p>-</p>
-          <p>-</p>
-          <p>-</p>
+          <ul class="list-disc ml-5">
+            <li>Jamur patogen</li>
+            <li>Munculnya bercak-bercak kecil</li>
+            <li>Daun akan menguning dan menggulung</li>
+          </ul>
         </div>
     </div>
     
@@ -96,9 +111,11 @@ const DISEASE = {
           Solusi
         </div>
         <div class="collapse-content">
-          <p>-</p>
-          <p>-</p>
-          <p>-</p>
+          <ul class="list-disc ml-5">
+            <li>Berikan obat Rabcide 50 WP</li>
+            <li>Jarak tanam tidak terlalu rapat pada musim hujan</li>
+            <li>Pemupukan berimbang yang lengkap, yaitu 250 kg urea, 100 kg SP36, dan 100 kg KCl per ha</li>
+          </ul>
         </div>
     </div>
     `
--- resources/views/core/index.blade.php
+++ resources/views/core/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="mx-4 my-2">
   <div class="card md:card-side w-full bg-base-100 shadow-xl">
-    <figure class="max-w-lg"><img id="preview" class="max-h-screen w-full object-cover"/></figure>
+    <figure class="md:max-w-lg"><img id="preview" class="max-h-screen w-full object-cover"/></figure>
     <div class="card-body justify-between">
       <div class="radial-progress mx-auto" style="--value:0;">0%</div>
       <progress class="progress hidden progress-primary w-full" value="50" max="100"></progress>
@@ -80,7 +80,7 @@
 
       setTimeout(() => {
         predictImage()
-      }, 100)
+      }, 200)
     }
   })
 
